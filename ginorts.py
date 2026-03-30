6a6903931328a6c9bfe19ee26a01529e1623fe3f
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-30 21:15:20 +0530

    Create ginorts.py

---
+++ ginorts.py
@@ -0,0 +1,6 @@
+s = input()
+lower=sorted([c for c in s if c.islower()])
+upper=sorted([c for c in s if c.isupper()])
+odd=sorted([c for c in s if c.isdigit() and int(c) % 2 != 0])
+even=sorted([c for c in s if c.isdigit() and int(c) % 2 == 0])
+print("".join(lower + upper + odd + even))
