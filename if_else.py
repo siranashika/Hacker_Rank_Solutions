dd66e509bf8331cd04abb743eaa2d9017e197089
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-30 20:15:11 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2!=0 or n in range(6,21):
+        print("Weird")
+    else:
+        if n in range(2,6) or n>20:
+            print("Not Weird")
