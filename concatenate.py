4b012d9671ee2e05599d59a0e0e9a41c44a63fab
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-30 21:48:25 +0530

    Create concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,5 @@
+import numpy
+n, m, p = map(int, input().split())
+array_1 = numpy.array([input().split() for _ in range(n)], int)
+array_2 = numpy.array([input().split() for _ in range(m)], int)
+print(numpy.concatenate((array_1, array_2), axis=0))
