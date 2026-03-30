2795cb978a8e36a3863bdbcd53499f41d1404927
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-30 20:39:47 +0530

    Create set_difference().operation.py

---
+++ set_difference().operation.py
@@ -0,0 +1,6 @@
+n_eng = int(input())
+eng_subs = set(map(int, input().split()))
+n_fre = int(input())
+fre_subs = set(map(int, input().split()))
+only_english = eng_subs.difference(fre_subs)
+print(len(only_english))
